56118283d53b270bcbf789c49206392ac219f6b4
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-05-21 13:45:35 -0700

    refactored package name

--- EcareWebApp/src/main/java/com/ecare/EcareWebAppApplication.java
+++ EcareWebApp/src/main/java/com/ecare/EcareWebAppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class EcareWebbAppApplication {
+public class EcareWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcareWebAppApplication.class, args);
