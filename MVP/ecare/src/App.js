486e34fc79f8aef1a68325bd1e1f08c128ba67e9
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-06-04 14:57:04 -0700

    Adding Front-End Skeleton

--- MVP/ecare/src/App.js
+++ MVP/ecare/src/App.js
@@ -4,13 +4,29 @@ function App() {
   return (
     <section class="body">
       	<img class="icon" src="EcareIcon.png"/>
-        <section class="exchange">
-          <div id="left/top-wallet">
+        <section class="exchange-grid">
+          <div id="left-top-wallet">
 
+            <button type="button" id="connect-wallet">
+              Connect Wallet
+            </button>
+            <p id="wallet-connected">
+              Wallet Connected
+            </p>
           </div>
-          <div id="right/bottom-wallet">
+          
+          <div id="right-bottom-wallet">
 
+            <button type="button" id="connect-wallet">
+              Connect Wallet
+            </button>
+            <p id="wallet-connected">
+              Wallet Connected
+            </p>
           </div>
+          <button type="button" id="swap">
+            SWAP
+          </button>
         </section>
 
     </section>
