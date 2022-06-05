af659f0e70c431b79453ff058ce721f04646ae8f
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-06-04 18:32:21 -0700

    Baser layer of Prototype Front-End completed

--- MVP/ecare/src/App.js
+++ MVP/ecare/src/App.js
@@ -3,32 +3,42 @@ import './App.css';
 function App() {
   return (
     <section class="body">
-      	<img class="icon" src="EcareIcon.png"/>
+      <a class="icon">
+        <img src="EcareIcon.png"/>
+      </a>
+
         <section class="exchange-grid">
           <div id="left-top-wallet">
 
             <button type="button" id="connect-wallet">
-              Connect Wallet
+              Connect Wallet One
             </button>
             <p id="wallet-connected">
-              Wallet Connected
+              Wallet One Connected
             </p>
           </div>
-          
+
           <div id="right-bottom-wallet">
 
             <button type="button" id="connect-wallet">
-              Connect Wallet
+              Connect Wallet Two
             </button>
             <p id="wallet-connected">
-              Wallet Connected
+              Wallet Two Connected
             </p>
           </div>
-          <button type="button" id="swap">
-            SWAP
-          </button>
         </section>
-
+        <button type="button" id="exchange">
+            EXCHANGE
+        </button>
+        <hr></hr>
+        <h3 id="name">
+          <p id="names">ethereum</p>
+          <p id="names">combinatorial</p>
+          <p id="names">auction</p>
+          <p id="names">rate</p>
+          <p id="names">exchange</p>
+        </h3>
     </section>
   );
 }
