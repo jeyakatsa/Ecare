cb8ded048824497c099d74ef13072f5baa83c34e
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-06-05 18:04:58 -0700

    Prototype UI Base completed

--- MVP/ecare/src/App.js
+++ MVP/ecare/src/App.js
@@ -3,78 +3,104 @@ import './App.css';
 function App() {
   return (
     <section class="body">
+
       <a class="icon">
-        <img src="EcareIcon.png"/>
+        <img id="img" src="EcareIcon.png"/>
       </a>
 
-        <section class="exchange-grid">
-          <div id="left-top-wallet">
-            <form action="/connect-wallet" method="post" modelAttribute="charity">
+      <section class="exchange-grid">
+
+        <div id="left-top-wallet">
+          <form action="/connect-wallet" method="post" modelAttribute="charity">
+            
+            <button type="button" id="choose-token">
+              <img id="ethicon" src="eth-icon.png"/>ETH
+            </button>
+            
+            <div class="form-group" id="enter-content">
+              <label id="label" path="name">Balance</label>
+              <br></br>
+              <errors id="errors" path="name"></errors>
+              <label id="numbers" path="name">$0.00</label>
+            </div>
+            
+            <div class="form-group" id="enter-content">
+              <label id="label" path="website">Total Tokens</label>					
+              <br></br>
+              <errors id="errors" path="website"></errors>
+              <label id="numbers" path="name">0.00</label>
+            </div>
+            
+            <div class="form-group" id="enter-content">
+              <label id="label" path="email">Exchange Tokens</label>						
+              <br></br>
+              <errors id="errors" path="email"></errors>
+              <input id="input" class="form-control" path="email"></input>
+            </div>
+
+            <br></br>
+
+              <input id="connect-wallet" type="submit" value="Connect Wallet One"/>
+          
+          </form>
+          <p id="wallet-connected">
+            Wallet One Connected
+          </p>
+        </div>
+
+        <div id="right-bottom-wallet">
+          <form action="/connect-wallet" method="post" modelAttribute="charity">
+
+              <button type="button" id="choose-token">
+                <img id="ethicon" src="weth-icon.png"/>WETH
+              </button>
+
               <div class="form-group" id="enter-content">
                 <label id="label" path="name">Balance</label>
                 <br></br>
                 <errors id="errors" path="name"></errors>
                 <label id="numbers" path="name">$0.00</label>
               </div>
+              
               <div class="form-group" id="enter-content">
                 <label id="label" path="website">Total Tokens</label>					
                 <br></br>
                 <errors id="errors" path="website"></errors>
                 <label id="numbers" path="name">0.00</label>
               </div>
+              
               <div class="form-group" id="enter-content">
                 <label id="label" path="email">Exchange Tokens</label>						
                 <br></br>
                 <errors id="errors" path="email"></errors>
                 <input id="input" class="form-control" path="email"></input>
               </div>										
+              
               <br></br>
-                <input id="connect-wallet" type="submit" value="Connect Wallet One"/>
-            </form>
-            <p id="wallet-connected">
-              Wallet One Connected
-            </p>
-          </div>
 
-          <div id="right-bottom-wallet">
-            <form action="/connect-wallet" method="post" modelAttribute="charity">
-                <div class="form-group" id="enter-content">
-                  <label id="label" path="name">Balance</label>
-                  <br></br>
-                  <errors id="errors" path="name"></errors>
-                  <label id="numbers" path="name">$0.00</label>
-                </div>
-                <div class="form-group" id="enter-content">
-                  <label id="label" path="website">Total Tokens</label>					
-                  <br></br>
-                  <errors id="errors" path="website"></errors>
-                  <label id="numbers" path="name">0.00</label>
-                </div>
-                <div class="form-group" id="enter-content">
-                  <label id="label" path="email">Exchange Tokens</label>						
-                  <br></br>
-                  <errors id="errors" path="email"></errors>
-                  <input id="input" class="form-control" path="email"></input>
-                </div>										
-                <br></br>
-                  <input id="connect-wallet" type="submit" value="Connect Wallet One"/>
-              </form>
-            <p id="wallet-connected">
-              Wallet Two Connected
-            </p>
-          </div>
-        </section>
-        <button type="button" id="exchange">
-            EXCHANGE
-        </button>
-        <hr></hr>
-        <h3 id="name">
-          <p id="names">ethereum</p>
-          <p id="names">combinatorial</p>
-          <p id="names">auction</p>
-          <p id="names">rate</p>
-          <p id="names">exchange</p>
-        </h3>
+                <input id="connect-wallet" type="submit" value="Connect Wallet Two"/>
+            </form>
+          <p id="wallet-connected">
+            Wallet Two Connected
+          </p>
+        </div>
+
+      </section>
+
+      <button type="button" id="exchange">
+          EXCHANGE
+      </button>
+
+      <hr></hr>
+
+      <h3 id="name">
+        <p id="names">ethereum</p>
+        <p id="names">combinatorial</p>
+        <p id="names">auction</p>
+        <p id="names">rate</p>
+        <p id="names">exchange</p>
+      </h3>
+
     </section>
   );
 }
