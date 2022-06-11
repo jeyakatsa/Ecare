6a3ad97058674a24a56e58f2f508323a14f48df4
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-06-11 08:36:20 -0700

    Mechanism Model created

--- MVP/EcareWebApp/src/main/java/com/ecare/models/Mechanism.java
+++ MVP/EcareWebApp/src/main/java/com/ecare/models/Mechanism.java
@@ -1,5 +1,39 @@
 package com.ecare.models;
 
-public class Mechanism {
+import javax.validation.constraints.NotBlank;
 
+public class Mechanism {
+	private Double balance;
+	private Double totalTokens;
+	@NotBlank (message="Required")
+	private Double exchangeTokens;
+	
+	public Mechanism() {
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getTotalTokens() {
+		return totalTokens;
+	}
+
+	public void setTotalTokens(Double totalTokens) {
+		this.totalTokens = totalTokens;
+	}
+
+	public Double getExchangeTokens() {
+		return exchangeTokens;
+	}
+
+	public void setExchangeTokens(Double exchangeTokens) {
+		this.exchangeTokens = exchangeTokens;
+	}
+	
+	
 }
