d387be18fb03bd9c33cc0b7f9597afd40a764dfe
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-06-12 11:47:32 -0700

    Building Model Attribute

--- MVP/EcareWebApp/src/main/java/com/ecare/controllers/MainController.java
+++ MVP/EcareWebApp/src/main/java/com/ecare/controllers/MainController.java
@@ -1,15 +1,32 @@
 package com.ecare.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecare.models.WalletOne;
+import com.ecare.services.ExchangeService;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 	
+	@Autowired
+	private ExchangeService exchangeService;
+	
+	public MainController(ExchangeService exchangeService) {
+		this.exchangeService = exchangeService;
+	}
+	
 	@GetMapping("")
-	public String Index() {
+	public String getWalletOne(@ModelAttribute("walletOne") WalletOne walletOne, Model model) {
+		List<WalletOne> walletOneNumbers = this.exchangeService.getWalletOne();
+		model.addAttribute("walletOne", walletOneNumbers);
 		return "index.jsp";
 	}
 	
