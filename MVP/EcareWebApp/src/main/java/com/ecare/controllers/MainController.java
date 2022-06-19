256433ac6d58f8ccb6f3dfea181112c0ea44a2f6
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-06-18 20:04:07 -0700

    Cleaned up packages

--- MVP/EcareWebApp/src/main/java/com/ecare/controllers/MainController.java
+++ MVP/EcareWebApp/src/main/java/com/ecare/controllers/MainController.java
@@ -9,20 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ecare.models.WalletOne;
-import com.ecare.models.WalletTwo;
-import com.ecare.services.ExchangeService;
-
 @Controller
 @RequestMapping("/")
 public class MainController {
 	
-	@Autowired
-	private ExchangeService exchangeService;
-	
-	public MainController(ExchangeService exchangeService) {
-		this.exchangeService = exchangeService;
-	}
 	
 	@GetMapping("")
 	public String index() {
@@ -41,16 +31,10 @@ public class MainController {
 		return "test/index-test.jsp";
 	}
 	
-//	@GetMapping("/prototype-test")
-//	public String prototypeTest() {
-//		return "test/prototype-test.jsp";
-//	}
-//	
-	
 	@GetMapping("/prototype-test")
-	public String GetWalletOne(@ModelAttribute("walletOne") WalletOne walletOne, Model model) {
-		Double numbers = this.exchangeService.getWalletOne();
-		model.addAttribute("walletOne", numbers);
+	public String prototypeTest() {
 		return "test/prototype-test.jsp";
 	}
+	
+
 }
