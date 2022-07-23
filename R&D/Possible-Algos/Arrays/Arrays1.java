89e1d0b8cfbd3f5658ddc861e9c4292ac6ea5fb8
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-07-22 20:54:08 -0700

    Added Hash Table function to Arrays Algo

--- R&D/Possible-Algos/Arrays/Arrays1.java
+++ R&D/Possible-Algos/Arrays/Arrays1.java
@@ -8,4 +8,20 @@ public class Arrays1 {
 
 
   //TWO SUM
-  //O(n^2) brute force solution...
+  //O(n) hash table solution
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
+        }
+    }
+    // In case there is no solution, we'll just return null
+    return null;
+  }
+
+}
