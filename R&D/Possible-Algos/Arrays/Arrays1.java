940e63f35e8cdf2777a0513af1d72e4fdeeb8b16
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-07-24 17:23:24 -0700

    Added new Algorithm Sample to Arrays

--- R&D/Possible-Algos/Arrays/Arrays1.java
+++ R&D/Possible-Algos/Arrays/Arrays1.java
@@ -24,4 +24,22 @@ public class Arrays1 {
     return null;
   }
 
+
+  //Container with Most Water
+  public int maxArea(int[] height) {
+    int maxarea = 0;
+    int left = 0; 
+    int right = height.length - 1;
+    while (left < right) {
+        int width = right - left;
+        maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * width);
+        if (height[left] <= height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxarea;
+  }  
+
 }
