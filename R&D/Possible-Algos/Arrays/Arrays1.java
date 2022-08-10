765c3073c3d445c288fa07758a0b0a13bd285035
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-08-09 17:33:18 -0700

    Polishing up Algorithms

--- R&D/Possible-Algos/Arrays/Arrays1.java
+++ R&D/Possible-Algos/Arrays/Arrays1.java
@@ -22,23 +22,6 @@ public class Arrays1 {
     }
     // In case there is no solution, we'll just return null
     return null;
-    }
-
-    //Container with Most H2O
-    public int maxArea(int[] height) {
-        int maxarea = 0;
-        int left = 0; 
-        int right = height.length - 1;
-        while (left < right) {
-            int width = right - left;
-            maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * width);
-            if (height[left] <= height[right]) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        return maxarea;
-    }
+}
 
 }
