1a3e5a43c82caba70a952f977d6deba0bff831ef
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-08-12 17:39:55 -0700

    Added Merge Intervals Algorithm

--- R&D/Possible-Algos/Arrays/Arrays1.java
+++ R&D/Possible-Algos/Arrays/Arrays1.java
@@ -50,27 +50,46 @@ public class Arrays1 {
         dfs(grid, r + 1, c);
         dfs(grid, r, c - 1);
         dfs(grid, r, c + 1);
-      }
+    }
     
-      public int numIslands(char[][] grid) {
+    public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) {
-          return 0;
+            return 0;
         }
-    
+
         int nr = grid.length;
         int nc = grid[0].length;
         int num_islands = 0;
         for (int r = 0; r < nr; ++r) {
-          for (int c = 0; c < nc; ++c) {
-            if (grid[r][c] == '1') {
-              ++num_islands;
-              dfs(grid, r, c);
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
             }
-          }
         }
-    
+
         return num_islands;
-      }    
+    }    
+
+    //Merge Intervals
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }    
        
 
 }
