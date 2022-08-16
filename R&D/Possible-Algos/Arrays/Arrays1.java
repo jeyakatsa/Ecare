7f52e03c1a671c9df5d6a83f8ec07b172b0cba8a
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-08-15 17:53:46 -0700

    Added Crypto Buy/Sell Algo

--- R&D/Possible-Algos/Arrays/Arrays1.java
+++ R&D/Possible-Algos/Arrays/Arrays1.java
@@ -103,7 +103,20 @@ public class Arrays1 {
         }
         
         return maxSubarray;
-    }    
+    }
+
+    //Buy/Sell Crypto/Stock
+    public int maxProfit(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        return maxprofit;
+    }        
        
 
 }
