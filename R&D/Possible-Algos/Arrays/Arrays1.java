aed547ff4151b7e5ba4a7f50011161d725eee8b8
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-08-18 17:50:16 -0700

    Added Subarray Algorithm

--- R&D/Possible-Algos/Arrays/Arrays1.java
+++ R&D/Possible-Algos/Arrays/Arrays1.java
@@ -172,6 +172,21 @@ public class Arrays1 {
         }
 
         return minDiff;
+    }    
+
+    //Subarray Sum Equals K
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++)
+                    sum += nums[i];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
     }               
        
 
