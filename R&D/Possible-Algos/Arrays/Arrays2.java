171612369117e52eb2e08599ec0d12fa45307f71
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-08-24 17:15:24 -0700

    Meeting Rooms II Algorithm Added

--- R&D/Possible-Algos/Arrays/Arrays2.java
+++ R&D/Possible-Algos/Arrays/Arrays2.java
@@ -2,139 +2,52 @@ import java.util.*;
 
 public class Arrays2 {
 
-    //Next Permutation
-    public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums, i, j);
-        }
-        reverse(nums, i + 1);
-    }
-    private void reverse(int[] nums, int start) {
-        int i = start, j = nums.length - 1;
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
-    }
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }  
-
-
-    //Kth Largest Element
-    public int findKthLargest(int[] nums, int k) {
-        // init heap 'the smallest element first'
-        PriorityQueue<Integer> heap =
-            new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
-
-        // keep k largest elements in the heap
-        for (int n: nums) {
-          heap.add(n);
-          if (heap.size() > k)
-            heap.poll();
-        }
-
-        // output
-        return heap.poll();        
-    }    
-
-
-    //Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
-    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        Arrays.sort(horizontalCuts);
-        Arrays.sort(verticalCuts);
-        int n = horizontalCuts.length;
-        int m = verticalCuts.length;
+    //Meeting Rooms II
+    public int minMeetingRooms(int[][] intervals) {
         
-        long maxHeight = Math.max(horizontalCuts[0], h - horizontalCuts[n - 1]);
-        for (int i = 1; i < n; i++) {
-
-            // horizontalCuts[i] - horizontalCuts[i - 1] represents the distance between
-            // two adjacent edges, and thus a possible height
-            maxHeight = Math.max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
+        // Check for the base case. If there are no intervals, return 0
+        if (intervals.length == 0) {
+          return 0;
         }
-        
-        // Consider the edges first
-        long maxWidth = Math.max(verticalCuts[0], w - verticalCuts[m - 1]);
-        for (int i = 1; i < m; i++){
-            // verticalCuts[i] - verticalCuts[i - 1] represents the distance between
-            // two adjacent edges, and thus a possible width
-            maxWidth = Math.max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
+    
+        // Min heap
+        PriorityQueue<Integer> allocator =
+            new PriorityQueue<Integer>(
+                intervals.length,
+                new Comparator<Integer>() {
+                  public int compare(Integer a, Integer b) {
+                    return a - b;
+                  }
+                });
+    
+        // Sort the intervals by start time
+        Arrays.sort(
+            intervals,
+            new Comparator<int[]>() {
+              public int compare(final int[] a, final int[] b) {
+                return a[0] - b[0];
+              }
+            });
+    
+        // Add the first meeting
+        allocator.add(intervals[0][1]);
+    
+        // Iterate over remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
+    
+          // If the room due to free up the earliest is free, assign that room to this meeting.
+          if (intervals[i][0] >= allocator.peek()) {
+            allocator.poll();
+          }
+    
+          // If a new room is to be assigned, then also we add to the heap,
+          // If an old room is allocated, then also we have to add to the heap with updated end time.
+          allocator.add(intervals[i][1]);
         }
-
-        // Be careful of overflow, and don't forget the modulo!
-        return (int) ((maxWidth * maxHeight) % (1000000007));
-    }    
-
-
-
-    //Spiral Matrix
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        int up = 0;
-        int left = 0;
-        int right = columns - 1;
-        int down = rows - 1;
-
-        while (result.size() < rows * columns) {
-            // Traverse from left to right.
-            for (int col = left; col <= right; col++) {
-                result.add(matrix[up][col]);
-            }
-            // Traverse downwards.
-            for (int row = up + 1; row <= down; row++) {
-                result.add(matrix[row][right]);
-            }
-            // Make sure we are now on a different row.
-            if (up != down) {
-                // Traverse from right to left.
-                for (int col = right - 1; col >= left; col--) {
-                    result.add(matrix[down][col]);
-                }
-            }
-            // Make sure we are now on a different column.
-            if (left != right) {
-                // Traverse upwards.
-                for (int row = down - 1; row > up; row--) {
-                    result.add(matrix[row][left]);
-                }
-            }
-            left++;
-            right--;
-            up++;
-            down--;
-        }
-
-        return result;
-    }   
-
-
-
-    //Container With Most Water
-    public int maxArea(int[] height) {
-        int maxarea = 0, l = 0, r = height.length - 1;
-        while (l < r) {
-            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
-            if (height[l] < height[r])
-                l++;
-            else
-                r--;
-        }
-        return maxarea;
-    }    
+    
+        // The size of the heap tells us the minimum rooms required for all the meetings.
+        return allocator.size();
+      }    
 
 
 }
