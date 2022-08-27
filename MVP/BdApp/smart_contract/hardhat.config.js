e26aeb981035ad1769c0e6bf074f48ec96d6303f
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-08-27 14:33:58 -0700

    Working on Hardhat Config

--- MVP/BdApp/smart_contract/hardhat.config.js
+++ MVP/BdApp/smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
+// Ropsten Linke to Be Added After Eth Address correlated
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    ropsten: {
+      url: //To Be Added
+      accounts: ['']
+    }
+  }
 };
