7f7b8c7ac4eb5d02115be2e082a68fbecdc9ed90
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-09-07 18:51:36 -0700

    Added getter to Transaction Context

--- MVP/BdApp/client/src/context/TransactionContext.jsx
+++ MVP/BdApp/client/src/context/TransactionContext.jsx
@@ -7,3 +7,17 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
+const getEtheremContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transcactionContract = new ethers.contract(
+        contractAddress,
+        contractABI,
+        signer
+    );
+
+    console.log({
+        provider,
+        signer,
+    })
+}
