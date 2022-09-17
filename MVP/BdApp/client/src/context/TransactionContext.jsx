590036f33bbd6a7c8d4341edd6f7a77f371a3268
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-09-17 08:28:51 -0700

    Completed TransactionTontext Basin

--- MVP/BdApp/client/src/context/TransactionContext.jsx
+++ MVP/BdApp/client/src/context/TransactionContext.jsx
@@ -45,13 +45,19 @@ export const TransactionProvider = ({ children }) => {
     const connectWallet = async () => {
         try {
             if(!ethereum) return alert("Please install metamask");
-        } catch (error) {
 
+            const accounts = await ethereum/request({ method: 'eth_requestAccounts'});
+
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+
+            throw new Error("No ethereum object.");
         }
     }
 
     return (
-        <TransactionContext.Provider value={{ value: 'test' }}>
+        <TransactionContext.Provider value={{ connectWallet }}>
             {children}
         </TransactionContext.Provider>
     );
