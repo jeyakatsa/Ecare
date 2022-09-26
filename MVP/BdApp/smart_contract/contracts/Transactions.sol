507a061df84ff333392e35b8bad9874ef37314bb
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-09-26 16:52:54 -0700

    Added Transfer Event

--- MVP/BdApp/smart_contract/contracts/Transactions.sol
+++ MVP/BdApp/smart_contract/contracts/Transactions.sol
@@ -3,5 +3,17 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    //Code ro be rafactored
+    uint256 transactionCounter;
+
+    event Transfer(
+        address from,
+        address receiver,
+        uint amount,
+        string message,
+        uint timestamp,
+        string keyword
+    );
+
+    
+
 }
