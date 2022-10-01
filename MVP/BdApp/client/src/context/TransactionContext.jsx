0ab7e0a8a4fd3e521624130de9a292077173d5a9
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-09-30 21:27:28 -0700

    Until Tomorrow

--- MVP/BdApp/client/src/context/TransactionContext.jsx
+++ MVP/BdApp/client/src/context/TransactionContext.jsx
@@ -9,4 +9,18 @@ export const TransactionContext = React.createContext();
 const { ethereum } = window;
 
 const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider{
+        ethereum};
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+    );
+
+    console.log({
+        provider,
+        signer,
+        transactionContract
+    })
 }
